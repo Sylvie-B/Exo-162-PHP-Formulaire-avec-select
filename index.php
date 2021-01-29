d7cbb008032376109cd1ee0fe7a82d19ec0f5fb3
Sylvie-B <unsolo@hotmail.fr>
2021-01-29 15:52:32 +0100

    exo 162 exo 5

--- index.php
+++ index.php
@@ -9,28 +9,60 @@
 </head>
 <body>
     <div>
+        <p>Exo 1</p>
         <form method="get" action="user.php">
+
             <div>
-                <label for="name">nom :</label>
-                <input type="text" name="userName" id="name">
+                <label for="nameEx1">nom :</label>
+                <input type="text" name="userName1" id="nameEx1">
             </div>
             <div>
-                <label for="surname">prénom :</label>
-                <input type="text" name="userSurname" id="surname">
+                <label for="surnameEx1">prénom :</label>
+                <input type="text" name="userSurname1" id="surnameEx1">
             </div>
             <div>
                 <input type="submit" value="Envoyer">
             </div>
         </form>
-
+    </div>
+    <div>
+        <p>Exo 2</p>
         <form method="post" action="user.php">
             <div>
-                <label for="name">nom :</label>
-                <input type="text" name="userName" id="name">
+                <label for="nameEx2">nom :</label>
+                <input type="text" name="userName2" id="nameEx2">
             </div>
             <div>
-                <label for="surname">prénom :</label>
-                <input type="text" name="userSurname" id="surname">
+                <label for="surnameEx2">prénom :</label>
+                <input type="text" name="userSurname2" id="surnameEx2">
+            </div>
+            <div>
+                <input type="submit" value="Envoyer">
+            </div>
+        </form>
+    </div>
+    <div>
+        <p>Exo 5</p>
+        <form method="post" action="index.php" enctype="multipart/form-data">
+            <div>
+                <label for="civ">civilité :</label>
+                <select name="civ_5" id="civ">
+                    <option value="Mr">Mr</option>
+                    <option value="Mme">Mme</option>
+                </select>
+
+            </div>
+            <div>
+                <label for="nameEx5">nom :</label>
+                <input type="text" name="userName5" id="nameEx5">
+            </div>
+            <div>
+                <label for="surnameEx5">prénom :</label>
+                <input type="text" name="userSurname5" id="surnameEx5">
+            </div>
+            <div>
+                <label for="file">Fichier</label>
+                <input type="file" name="userFile" id="file">
             </div>
             <div>
                 <input type="submit" value="Envoyer">
--- user.php
+++ user.php
@@ -1,1 +1,14 @@
 <?php
+
+if(isset($_GET['userName1'], $_GET['userSurname1'])){
+    echo $_GET['userName1']." ".$_GET['userSurname1'];
+}
+elseif (isset($_POST['userName2'], $_POST['userSurname2'])){
+    echo $_POST['userName2']." ".$_POST['userSurname2'];
+}
+elseif (isset($_POST['civ_5'], $_POST['userName5'], $_POST['userSurname5'])){
+    echo $_POST['civ_5']." ".$_POST['userName5']." ".$_POST['userSurname5'];
+}
+else {
+    header('location: index.php?error=1');
+}
